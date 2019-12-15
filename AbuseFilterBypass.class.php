8bcee1b6e99bd76d94955e915a9bfc96cef4c0df
Jack Phoenix <ashley@uncyclomedia.co>
2019-12-15 14:05:47 +0200

    ShoutWiki changes -- extension registration support, modernization, etc.
    Requires AbuseFilter commit 7edf12dbc6d9421d668619acd4abfe68af081f41 in order to work properly.
    
    Change-Id: Ida7a7a1f45b924dabdae7bb2610576859ea0c50b

--- AbuseFilterBypass.class.php
+++ AbuseFilterBypass.class.php
@@ -2,19 +2,25 @@
 /**
  * AbuseFilterBypass
  *
- * Allows staff to bypass AbuseFilter checks
+ * Allows privileged users to bypass AbuseFilter checks
  *
  * @author Nelson Monterroso <nelson@wikia-inc.com>
+ * @author Jack Phoenix -- modernization etc. on 31 July 2019
  */
 
 class AbuseFilterBypass {
 	/**
-	 * determine if a user should be allowed to skip filter checks
-	 * @param User $user
+	 * Determine if a user should be allowed to skip filter checks
+	 *
+	 * @param AbuseFilterVariableHolder $vars The variables for the current action
+	 * @param Title $title The title where the action being filtered was performed
+	 * @param User $user The user who performed the action being filtered
+	 * @param array &$skipReasons Array of reasons why the action should be skipped
 	 * @return bool
 	 */
-	public static function onBypassCheck( User $user ) {
+	public static function onBypassCheck( $vars, $title, User $user, &$skipReasons ) {
 		$skipFilters = $user->isAllowed( 'abusefilter-bypass' );
+		$skipReasons[] = "user has 'abusefilter-bypass' user right";
 		return !$skipFilters; // since we check that these hooks return false
 	}
 }
